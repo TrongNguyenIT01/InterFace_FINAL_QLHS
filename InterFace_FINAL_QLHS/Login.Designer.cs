ffd070d77d3c66e1d5e38e1a81d80990cd793cb2
kien <huagiakien123@gmail.com>
2026-04-11 18:30:08 +0700

    Trang Login

--- InterFace_FINAL_QLHS/Login.Designer.cs
+++ InterFace_FINAL_QLHS/Login.Designer.cs
@@ -28,20 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.panelTrai = new System.Windows.Forms.Panel();
+            this.panelPhai = new System.Windows.Forms.Panel();
+            this.lblTieude = new System.Windows.Forms.Label();
+            this.txtDangnhap = new System.Windows.Forms.TextBox();
+            this.txtMatkhau = new System.Windows.Forms.TextBox();
+            this.cbVaitro = new System.Windows.Forms.ComboBox();
+            this.chkGhinho = new System.Windows.Forms.CheckBox();
+            this.btnDangnhap = new System.Windows.Forms.Button();
+            this.lblTendangnhap = new System.Windows.Forms.Label();
+            this.lblMatkhau = new System.Windows.Forms.Label();
+            this.lblVaitro = new System.Windows.Forms.Label();
+            this.linkQuenmk = new System.Windows.Forms.LinkLabel();
+            this.linkLienhequantrivien = new System.Windows.Forms.LinkLabel();
+            this.lblChuacotaikhoan = new System.Windows.Forms.Label();
+            this.panelPhai.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelTrai
+            // 
+            this.panelTrai.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelTrai.BackgroundImage")));
+            this.panelTrai.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelTrai.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelTrai.Location = new System.Drawing.Point(0, 0);
+            this.panelTrai.Name = "panelTrai";
+            this.panelTrai.Size = new System.Drawing.Size(400, 554);
+            this.panelTrai.TabIndex = 0;
+            // 
+            // panelPhai
+            // 
+            this.panelPhai.BackColor = System.Drawing.Color.White;
+            this.panelPhai.Controls.Add(this.lblChuacotaikhoan);
+            this.panelPhai.Controls.Add(this.linkLienhequantrivien);
+            this.panelPhai.Controls.Add(this.linkQuenmk);
+            this.panelPhai.Controls.Add(this.lblVaitro);
+            this.panelPhai.Controls.Add(this.lblMatkhau);
+            this.panelPhai.Controls.Add(this.lblTendangnhap);
+            this.panelPhai.Controls.Add(this.btnDangnhap);
+            this.panelPhai.Controls.Add(this.chkGhinho);
+            this.panelPhai.Controls.Add(this.cbVaitro);
+            this.panelPhai.Controls.Add(this.txtMatkhau);
+            this.panelPhai.Controls.Add(this.txtDangnhap);
+            this.panelPhai.Controls.Add(this.lblTieude);
+            this.panelPhai.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPhai.Location = new System.Drawing.Point(400, 0);
+            this.panelPhai.Name = "panelPhai";
+            this.panelPhai.Size = new System.Drawing.Size(667, 554);
+            this.panelPhai.TabIndex = 1;
+            // 
+            // lblTieude
+            // 
+            this.lblTieude.AutoSize = true;
+            this.lblTieude.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTieude.Location = new System.Drawing.Point(120, 9);
+            this.lblTieude.Name = "lblTieude";
+            this.lblTieude.Size = new System.Drawing.Size(390, 114);
+            this.lblTieude.TabIndex = 0;
+            this.lblTieude.Text = "Chào Mừng Quay Trở Lại 👋\r\n\r\n\r\n";
+            // 
+            // txtDangnhap
+            // 
+            this.txtDangnhap.Location = new System.Drawing.Point(29, 90);
+            this.txtDangnhap.Name = "txtDangnhap";
+            this.txtDangnhap.Size = new System.Drawing.Size(511, 22);
+            this.txtDangnhap.TabIndex = 1;
+            // 
+            // txtMatkhau
+            // 
+            this.txtMatkhau.Location = new System.Drawing.Point(29, 156);
+            this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.Size = new System.Drawing.Size(511, 22);
+            this.txtMatkhau.TabIndex = 2;
+            // 
+            // cbVaitro
+            // 
+            this.cbVaitro.AllowDrop = true;
+            this.cbVaitro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbVaitro.FormattingEnabled = true;
+            this.cbVaitro.Items.AddRange(new object[] {
+            "Admin",
+            "Giáo Viên",
+            "Giáo Vụ"});
+            this.cbVaitro.Location = new System.Drawing.Point(29, 224);
+            this.cbVaitro.Name = "cbVaitro";
+            this.cbVaitro.Size = new System.Drawing.Size(511, 24);
+            this.cbVaitro.TabIndex = 3;
+            // 
+            // chkGhinho
+            // 
+            this.chkGhinho.AutoSize = true;
+            this.chkGhinho.Location = new System.Drawing.Point(29, 281);
+            this.chkGhinho.Name = "chkGhinho";
+            this.chkGhinho.Size = new System.Drawing.Size(141, 20);
+            this.chkGhinho.TabIndex = 4;
+            this.chkGhinho.Text = "Ghi nhớ đăng nhập";
+            this.chkGhinho.UseVisualStyleBackColor = true;
+            // 
+            // btnDangnhap
+            // 
+            this.btnDangnhap.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnDangnhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangnhap.ForeColor = System.Drawing.Color.White;
+            this.btnDangnhap.Location = new System.Drawing.Point(126, 331);
+            this.btnDangnhap.Name = "btnDangnhap";
+            this.btnDangnhap.Size = new System.Drawing.Size(363, 42);
+            this.btnDangnhap.TabIndex = 5;
+            this.btnDangnhap.Text = "Đăng Nhập";
+            this.btnDangnhap.UseVisualStyleBackColor = false;
+            // 
+            // lblTendangnhap
+            // 
+            this.lblTendangnhap.AutoSize = true;
+            this.lblTendangnhap.Location = new System.Drawing.Point(26, 71);
+            this.lblTendangnhap.Name = "lblTendangnhap";
+            this.lblTendangnhap.Size = new System.Drawing.Size(168, 16);
+            this.lblTendangnhap.TabIndex = 6;
+            this.lblTendangnhap.Text = "Tên đăng nhập hoặc Email";
+            // 
+            // lblMatkhau
+            // 
+            this.lblMatkhau.AutoSize = true;
+            this.lblMatkhau.Location = new System.Drawing.Point(26, 137);
+            this.lblMatkhau.Name = "lblMatkhau";
+            this.lblMatkhau.Size = new System.Drawing.Size(61, 16);
+            this.lblMatkhau.TabIndex = 7;
+            this.lblMatkhau.Text = "Mật khẩu";
+            // 
+            // lblVaitro
+            // 
+            this.lblVaitro.AutoSize = true;
+            this.lblVaitro.Location = new System.Drawing.Point(26, 205);
+            this.lblVaitro.Name = "lblVaitro";
+            this.lblVaitro.Size = new System.Drawing.Size(51, 16);
+            this.lblVaitro.TabIndex = 8;
+            this.lblVaitro.Text = "Vai Trò";
+            // 
+            // linkQuenmk
+            // 
+            this.linkQuenmk.AutoSize = true;
+            this.linkQuenmk.Location = new System.Drawing.Point(427, 285);
+            this.linkQuenmk.Name = "linkQuenmk";
+            this.linkQuenmk.Size = new System.Drawing.Size(97, 16);
+            this.linkQuenmk.TabIndex = 9;
+            this.linkQuenmk.TabStop = true;
+            this.linkQuenmk.Text = "Quên Mật Khẩu";
+            // 
+            // linkLienhequantrivien
+            // 
+            this.linkLienhequantrivien.AutoSize = true;
+            this.linkLienhequantrivien.Location = new System.Drawing.Point(302, 385);
+            this.linkLienhequantrivien.Name = "linkLienhequantrivien";
+            this.linkLienhequantrivien.Size = new System.Drawing.Size(134, 16);
+            this.linkLienhequantrivien.TabIndex = 10;
+            this.linkLienhequantrivien.TabStop = true;
+            this.linkLienhequantrivien.Text = "Liên hệ Quản Trị Viên";
+            // 
+            // lblChuacotaikhoan
+            // 
+            this.lblChuacotaikhoan.AutoSize = true;
+            this.lblChuacotaikhoan.Location = new System.Drawing.Point(162, 385);
+            this.lblChuacotaikhoan.Name = "lblChuacotaikhoan";
+            this.lblChuacotaikhoan.Size = new System.Drawing.Size(123, 16);
+            this.lblChuacotaikhoan.TabIndex = 11;
+            this.lblChuacotaikhoan.Text = "Chưa có tải khoản ?";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.panelPhai);
+            this.Controls.Add(this.panelTrai);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
-            this.Text = "Form1";
+            this.Text = "Login";
+            this.panelPhai.ResumeLayout(false);
+            this.panelPhai.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelTrai;
+        private System.Windows.Forms.Panel panelPhai;
+        private System.Windows.Forms.ComboBox cbVaitro;
+        private System.Windows.Forms.TextBox txtMatkhau;
+        private System.Windows.Forms.TextBox txtDangnhap;
+        private System.Windows.Forms.Label lblTieude;
+        private System.Windows.Forms.Label lblVaitro;
+        private System.Windows.Forms.Label lblMatkhau;
+        private System.Windows.Forms.Label lblTendangnhap;
+        private System.Windows.Forms.Button btnDangnhap;
+        private System.Windows.Forms.CheckBox chkGhinho;
+        private System.Windows.Forms.Label lblChuacotaikhoan;
+        private System.Windows.Forms.LinkLabel linkLienhequantrivien;
+        private System.Windows.Forms.LinkLabel linkQuenmk;
     }
 }
 
