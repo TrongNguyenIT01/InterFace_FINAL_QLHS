415a63ea8f0d2f3a3ffce14f16ae730724d49803
Phú <hoangphu24122005@gmail.com>
2026-04-12 21:28:32 +0700

    moi sua loi nho

--- InterFace_FINAL_QLHS/GiaoVu/TraCuuHocSinh.Designer.cs
+++ InterFace_FINAL_QLHS/GiaoVu/TraCuuHocSinh.Designer.cs
@@ -31,17 +31,18 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.txtTimHS = new System.Windows.Forms.TextBox();
             this.btnTimHS = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbKhoiLop = new System.Windows.Forms.ComboBox();
+            this.dgvDanhSachHocSinh = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSachHocSinh)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.cbKhoiLop);
             this.panel1.Controls.Add(this.btnTimHS);
             this.panel1.Controls.Add(this.txtTimHS);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
@@ -70,17 +71,6 @@
             this.btnTimHS.Text = "Tìm";
             this.btnTimHS.UseVisualStyleBackColor = false;
             // 
-            // listView1
-            // 
-            this.listView1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(0, 68);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(800, 382);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.label1);
@@ -100,22 +90,34 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "DANH SÁCH HỌC SINH";
             // 
-            // comboBox1
+            // cbKhoiLop
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(175, 27);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(137, 27);
-            this.comboBox1.TabIndex = 3;
+            this.cbKhoiLop.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cbKhoiLop.FormattingEnabled = true;
+            this.cbKhoiLop.Location = new System.Drawing.Point(175, 27);
+            this.cbKhoiLop.Name = "cbKhoiLop";
+            this.cbKhoiLop.Size = new System.Drawing.Size(137, 27);
+            this.cbKhoiLop.TabIndex = 3;
+            // 
+            // dgvDanhSachHocSinh
+            // 
+            this.dgvDanhSachHocSinh.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dgvDanhSachHocSinh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDanhSachHocSinh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDanhSachHocSinh.Location = new System.Drawing.Point(0, 117);
+            this.dgvDanhSachHocSinh.Name = "dgvDanhSachHocSinh";
+            this.dgvDanhSachHocSinh.RowHeadersWidth = 51;
+            this.dgvDanhSachHocSinh.RowTemplate.Height = 24;
+            this.dgvDanhSachHocSinh.Size = new System.Drawing.Size(800, 333);
+            this.dgvDanhSachHocSinh.TabIndex = 3;
             // 
             // TraCuuHocSinh
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDanhSachHocSinh);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.panel1);
             this.Name = "TraCuuHocSinh";
             this.Text = "TraCuuHocSinh";
@@ -123,6 +125,7 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSachHocSinh)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -132,9 +135,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox txtTimHS;
         private System.Windows.Forms.Button btnTimHS;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbKhoiLop;
+        private System.Windows.Forms.DataGridView dgvDanhSachHocSinh;
     }
 }
