ff3ec716a566975b13f7c2ffa8a5be53895bee88
Trong_Nguyen <lamnguyenkietco@gmail.com>
2026-04-23 23:17:42 +0700

    hihi

--- InterFace_FINAL_QLHS/GiaoVu/TraCuuHocSinh.Designer.cs
+++ InterFace_FINAL_QLHS/GiaoVu/TraCuuHocSinh.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.txtTimHS = new System.Windows.Forms.TextBox();
+            this.cbKhoiLop = new System.Windows.Forms.ComboBox();
             this.btnTimHS = new System.Windows.Forms.Button();
+            this.txtTimHS = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.cbKhoiLop = new System.Windows.Forms.ComboBox();
             this.dgvDanhSachHocSinh = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -47,78 +47,86 @@
             this.panel1.Controls.Add(this.txtTimHS);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 68);
+            this.panel1.Size = new System.Drawing.Size(766, 55);
             this.panel1.TabIndex = 0;
             // 
-            // txtTimHS
+            // cbKhoiLop
             // 
-            this.txtTimHS.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtTimHS.Location = new System.Drawing.Point(24, 27);
-            this.txtTimHS.Name = "txtTimHS";
-            this.txtTimHS.Size = new System.Drawing.Size(145, 27);
-            this.txtTimHS.TabIndex = 0;
+            this.cbKhoiLop.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cbKhoiLop.FormattingEnabled = true;
+            this.cbKhoiLop.Location = new System.Drawing.Point(131, 22);
+            this.cbKhoiLop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbKhoiLop.Name = "cbKhoiLop";
+            this.cbKhoiLop.Size = new System.Drawing.Size(104, 24);
+            this.cbKhoiLop.TabIndex = 3;
             // 
             // btnTimHS
             // 
             this.btnTimHS.BackColor = System.Drawing.Color.LightBlue;
             this.btnTimHS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTimHS.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnTimHS.Location = new System.Drawing.Point(713, 22);
+            this.btnTimHS.Location = new System.Drawing.Point(535, 18);
+            this.btnTimHS.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnTimHS.Name = "btnTimHS";
-            this.btnTimHS.Size = new System.Drawing.Size(75, 35);
+            this.btnTimHS.Size = new System.Drawing.Size(56, 28);
             this.btnTimHS.TabIndex = 2;
             this.btnTimHS.Text = "Tìm";
             this.btnTimHS.UseVisualStyleBackColor = false;
+            // 
+            // txtTimHS
+            // 
+            this.txtTimHS.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtTimHS.Location = new System.Drawing.Point(18, 22);
+            this.txtTimHS.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTimHS.Name = "txtTimHS";
+            this.txtTimHS.Size = new System.Drawing.Size(110, 23);
+            this.txtTimHS.TabIndex = 0;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.label1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 68);
+            this.panel2.Location = new System.Drawing.Point(0, 55);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 49);
+            this.panel2.Size = new System.Drawing.Size(766, 40);
             this.panel2.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(193, 3);
+            this.label1.Location = new System.Drawing.Point(145, 2);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(415, 39);
+            this.label1.Size = new System.Drawing.Size(334, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "DANH SÁCH HỌC SINH";
-            // 
-            // cbKhoiLop
-            // 
-            this.cbKhoiLop.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.cbKhoiLop.FormattingEnabled = true;
-            this.cbKhoiLop.Location = new System.Drawing.Point(175, 27);
-            this.cbKhoiLop.Name = "cbKhoiLop";
-            this.cbKhoiLop.Size = new System.Drawing.Size(137, 27);
-            this.cbKhoiLop.TabIndex = 3;
             // 
             // dgvDanhSachHocSinh
             // 
             this.dgvDanhSachHocSinh.BackgroundColor = System.Drawing.Color.Gainsboro;
             this.dgvDanhSachHocSinh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDanhSachHocSinh.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvDanhSachHocSinh.Location = new System.Drawing.Point(0, 117);
+            this.dgvDanhSachHocSinh.Location = new System.Drawing.Point(0, 95);
+            this.dgvDanhSachHocSinh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvDanhSachHocSinh.Name = "dgvDanhSachHocSinh";
             this.dgvDanhSachHocSinh.RowHeadersWidth = 51;
             this.dgvDanhSachHocSinh.RowTemplate.Height = 24;
-            this.dgvDanhSachHocSinh.Size = new System.Drawing.Size(800, 333);
+            this.dgvDanhSachHocSinh.Size = new System.Drawing.Size(766, 374);
             this.dgvDanhSachHocSinh.TabIndex = 3;
             // 
             // TraCuuHocSinh
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(766, 469);
             this.Controls.Add(this.dgvDanhSachHocSinh);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "TraCuuHocSinh";
             this.Text = "TraCuuHocSinh";
             this.panel1.ResumeLayout(false);
