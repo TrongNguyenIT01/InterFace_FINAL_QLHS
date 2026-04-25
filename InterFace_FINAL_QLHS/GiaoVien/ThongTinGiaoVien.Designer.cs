719c6e8be226fc519cd6cb5867d77273f6f412ad
Trong_Nguyen <lamnguyenkietco@gmail.com>
2026-04-25 23:17:45 +0700

    Hoàn Thành Thông Tin giáo viên

--- InterFace_FINAL_QLHS/GiaoVien/ThongTinGiaoVien.Designer.cs
+++ InterFace_FINAL_QLHS/GiaoVien/ThongTinGiaoVien.Designer.cs
@@ -28,93 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.lblThongTinGV = new System.Windows.Forms.Label();
-            this.dgvThongTinGV = new System.Windows.Forms.DataGridView();
             this.dgvLopDangDay = new System.Windows.Forms.DataGridView();
             this.lblLopDangDay = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvThongTinGV)).BeginInit();
+            this.dgvThongTinGV = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dgvLopDangDay)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThongTinGV)).BeginInit();
             this.SuspendLayout();
             // 
             // lblThongTinGV
             // 
             this.lblThongTinGV.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblThongTinGV.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblThongTinGV.Location = new System.Drawing.Point(299, 17);
+            this.lblThongTinGV.Location = new System.Drawing.Point(224, 13);
+            this.lblThongTinGV.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblThongTinGV.Name = "lblThongTinGV";
-            this.lblThongTinGV.Size = new System.Drawing.Size(201, 34);
+            this.lblThongTinGV.Size = new System.Drawing.Size(151, 28);
             this.lblThongTinGV.TabIndex = 0;
             this.lblThongTinGV.Text = "Thông Tin Giáo Viên";
-            // 
-            // dgvThongTinGV
-            // 
-            this.dgvThongTinGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvThongTinGV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
-            this.dgvThongTinGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvThongTinGV.DefaultCellStyle = dataGridViewCellStyle6;
-            this.dgvThongTinGV.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvThongTinGV.Location = new System.Drawing.Point(3, 71);
-            this.dgvThongTinGV.Name = "dgvThongTinGV";
-            this.dgvThongTinGV.RowHeadersWidth = 51;
-            this.dgvThongTinGV.RowTemplate.Height = 24;
-            this.dgvThongTinGV.Size = new System.Drawing.Size(794, 150);
-            this.dgvThongTinGV.TabIndex = 1;
             // 
             // dgvLopDangDay
             // 
             this.dgvLopDangDay.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvLopDangDay.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvLopDangDay.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dgvLopDangDay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvLopDangDay.DefaultCellStyle = dataGridViewCellStyle8;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvLopDangDay.DefaultCellStyle = dataGridViewCellStyle4;
             this.dgvLopDangDay.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvLopDangDay.Location = new System.Drawing.Point(3, 295);
+            this.dgvLopDangDay.Location = new System.Drawing.Point(2, 239);
+            this.dgvLopDangDay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvLopDangDay.Name = "dgvLopDangDay";
             this.dgvLopDangDay.RowHeadersWidth = 51;
             this.dgvLopDangDay.RowTemplate.Height = 24;
-            this.dgvLopDangDay.Size = new System.Drawing.Size(794, 152);
+            this.dgvLopDangDay.Size = new System.Drawing.Size(596, 125);
             this.dgvLopDangDay.TabIndex = 3;
             // 
             // lblLopDangDay
             // 
             this.lblLopDangDay.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblLopDangDay.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLopDangDay.Location = new System.Drawing.Point(328, 241);
+            this.lblLopDangDay.Location = new System.Drawing.Point(246, 195);
+            this.lblLopDangDay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblLopDangDay.Name = "lblLopDangDay";
-            this.lblLopDangDay.Size = new System.Drawing.Size(144, 34);
+            this.lblLopDangDay.Size = new System.Drawing.Size(108, 28);
             this.lblLopDangDay.TabIndex = 2;
             this.lblLopDangDay.Text = "Lớp Đang Dạy";
             // 
@@ -123,31 +96,43 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.dgvLopDangDay, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.dgvThongTinGV, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.lblThongTinGV, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.lblLopDangDay, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.dgvThongTinGV, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.27778F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.72222F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.27778F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.72222F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 366);
             this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // dgvThongTinGV
+            // 
+            this.dgvThongTinGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThongTinGV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvThongTinGV.Location = new System.Drawing.Point(3, 58);
+            this.dgvThongTinGV.Name = "dgvThongTinGV";
+            this.dgvThongTinGV.Size = new System.Drawing.Size(594, 121);
+            this.dgvThongTinGV.TabIndex = 4;
             // 
             // ThongTinGiaoVien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ThongTinGiaoVien";
             this.Text = "Thông Tin Giáo Viên";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvThongTinGV)).EndInit();
+            this.Load += new System.EventHandler(this.ThongTinGiaoVien_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvLopDangDay)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThongTinGV)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -155,9 +140,9 @@
         #endregion
 
         private System.Windows.Forms.Label lblThongTinGV;
-        private System.Windows.Forms.DataGridView dgvThongTinGV;
         private System.Windows.Forms.DataGridView dgvLopDangDay;
         private System.Windows.Forms.Label lblLopDangDay;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvThongTinGV;
     }
 }
--- InterFace_FINAL_QLHS/GiaoVien/ThongTinGiaoVien.cs
+++ InterFace_FINAL_QLHS/GiaoVien/ThongTinGiaoVien.cs
@@ -1,20 +1,201 @@
-﻿using System;
+﻿using InterFace_FINAL_QLHS.Config;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static InterFace_FINAL_QLHS.QuenMK;
 
 namespace InterFace_FINAL_QLHS.GiaoVien
 {
     public partial class ThongTinGiaoVien : Form
     {
+        string IDGV = Session.UserName;
         public ThongTinGiaoVien()
         {
             InitializeComponent();
+        }
+        private void Load_TT()
+        {
+            string sql = @"SELECT 
+                                gv.GiaoVienID,
+                                gv.HoTen,
+                                gv.NgaySinh,
+                                gv.GioiTinh,
+                                gv.DiaChi,
+                                gv.Email,
+                                gv.SoDienThoai,
+                                cm.TenCM,
+                                gv.TrangThai
+                           FROM GiaoVien gv
+                           JOIN ChuyenMon cm ON gv.MaCM = cm.MaCM
+                           WHERE gv.GiaoVienID = @IDGV";
+
+            SqlParameter[] param = {
+                new SqlParameter("@IDGV", IDGV)
+            };
+
+            DataTable dt = DataProvider.SelectData(sql, CommandType.Text, param);
+
+            dgvThongTinGV.AutoGenerateColumns = false;
+            dgvThongTinGV.DataSource = dt;
+
+            SetupDataGridView();
+        }
+
+        private void SetupDataGridView()
+        {
+            dgvThongTinGV.Columns.Clear();
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "GiaoVienID",
+                HeaderText = "ID",
+                Width = 80
+            });
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "HoTen",
+                HeaderText = "Họ Tên",
+                Width = 150
+            });
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "NgaySinh",
+                HeaderText = "Ngày Sinh",
+                DefaultCellStyle = { Format = "dd/MM/yyyy" },
+                Width = 100
+            });
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "GioiTinh",
+                HeaderText = "Giới Tính",
+                Width = 80
+            });
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "DiaChi",
+                HeaderText = "Địa Chỉ",
+                Width = 200
+            });
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "Email",
+                HeaderText = "Email",
+                Width = 150
+            });
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "SoDienThoai",
+                HeaderText = "SĐT",
+                Width = 100
+            });
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "TenCM",
+                HeaderText = "Chuyên Môn",
+                Width = 120
+            });
+
+            dgvThongTinGV.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "TrangThai",
+                HeaderText = "Trạng Thái",
+                Width = 100
+            });
+        }
+
+        private void dgvLopDangDay_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dgvThongTinGV.Columns[e.ColumnIndex].DataPropertyName == "GioiTinh")
+            {
+                if (e.Value != null)
+                {
+                    e.Value = (bool)e.Value ? "Nam" : "Nữ";
+                    e.FormattingApplied = true;
+                }
+            }
+
+            if (dgvThongTinGV.Columns[e.ColumnIndex].DataPropertyName == "TrangThai")
+            {
+                if (e.Value != null)
+                {
+                    e.Value = (bool)e.Value ? "Đang dạy" : "";
+                    e.FormattingApplied = true;
+                }
+            }
+
+
+        }
+        private void Load_LOP(string keyword = "")
+        {
+            try
+            {
+                // Lấy IDUser từ Session đã có sẵn
+                string maGV = UserSession.CurrentUsername;
+
+                // Câu lệnh SQL kết nối các bảng để lấy: Tên lớp, Tên môn, Sĩ số
+                string sql = @"
+                    SELECT 
+                        L.MaLop AS [Mã Lớp],
+                        L.TenLop AS [Tên Lớp], 
+                        MH.TenMon AS [Môn Giảng Dạy], 
+                        L.SiSo AS [Sĩ Số],
+                        HK.HocKy AS [Học Kỳ]
+                    FROM PhanCongGiangDay PC
+                    INNER JOIN Lop L ON PC.MaLop = L.MaLop
+                    INNER JOIN MonHoc MH ON PC.MaMon = MH.MaMon
+                    INNER JOIN HocKy HK ON PC.MaHK = HK.MaHK
+                    WHERE PC.GiaoVienID = @maGV";
+
+                // Nếu người dùng có nhập từ khóa tìm kiếm
+                if (!string.IsNullOrEmpty(keyword))
+                {
+                    // Thêm điều kiện tìm theo L.MaLop
+                    sql += " AND (L.TenLop LIKE @key OR MH.TenMon LIKE @key OR L.MaLop LIKE @key)";
+                }
+
+                // Tạo tham số
+                SqlParameter[] paras = new SqlParameter[]
+                {
+                    new SqlParameter("@maGV", maGV),
+                    new SqlParameter("@key", "%" + keyword + "%")
+                };
+
+                // Gọi DataProvider để lấy dữ liệu
+                DataTable dt = DataProvider.SelectData(sql, CommandType.Text, paras);
+
+                // Đổ vào DataGridView (giả sử tên là dgvDSLopDangDay)
+                dgvLopDangDay.DataSource = dt;
+
+                // Tùy chỉnh giao diện DataGridView một chút
+                if (dgvLopDangDay.Columns.Count > 0)
+                {
+                    dgvLopDangDay.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
+            }
+        }
+
+        private void ThongTinGiaoVien_Load(object sender, EventArgs e)
+        {
+            Load_TT();
+            Load_LOP();
         }
     }
 }
